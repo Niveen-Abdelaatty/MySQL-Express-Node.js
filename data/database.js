d87cf65a5ae467d6a3c40095729e00889970199e
Niveen Abdelaatty <niveen.abdelaatty@gmail.com>
2022-09-13 12:11:57 -0400

    update db connection

--- data/database.js
+++ data/database.js
@@ -2,11 +2,18 @@ const mysql = require('mysql2/promise');
 
 // Establish the database connection
 const pool = mysql.createPool({
+    connectionLimit: 100,
     host: 'localhost',
     database: 'blog',
     user: 'root',
     password: 'PortSaid_202'
 });
-
+pool.getConnection((err,connection)=> {
+    if(err)
+    throw err;
+    console.log('Database connected successfully');
+    connection.release();
+  });
+  
 // Export the db connection
 module.exports = pool;
